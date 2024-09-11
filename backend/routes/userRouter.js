30d790a1ecdef5f8bbcf39a733b853fba003d12c
Cha_mamme <solomonchamamme@gmail.com>
2024-09-11 20:29:05 +0000

    Created: user routes for the fetchings I will be updating them soon

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get('/', authController.login);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router.route('/:id').get(userController.getUser);
 
 module.exports = router;
