c33ab4b9ed98f09d65c4776c8717de97d9175c3b
Cha_mamme <solomonchamamme@gmail.com>
2024-09-26 14:01:46 +0000

    Added: the routes for the user athentication and authorization

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,5 +1,5 @@
-const authController = require('../controllers/authController');
 const express = require('express');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.post('/login', authController.login);
 router.post('/signup', authController.signUp);
 router.post('/forgot-password', authController.forgotPassword);
 router.patch('/reset-password/:token', authController.resetPassword);
-
+router.post('/verify/:token', authController.verifyUser);
 router.use(authController.protect);
 router.patch('/update-password', authController.updatePassword);
 module.exports = router;
