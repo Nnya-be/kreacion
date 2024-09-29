d2982d8f82d0ce106174fe1caec6683760937954
Cha_mamme <solomonchamamme@gmail.com>
2024-09-29 00:31:41 +0000

    Modified: The app.js for production ready code.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
 const userRouter = require('./routes/userRouter');
 const errorController = require('./controllers/errorController');
 const postRouter = require('./routes/postRouter');
@@ -9,7 +14,17 @@ const authRouter = require('./routes/authRouter');
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors({ origin: '*' }));
+app.use(hpp());
+app.use(xss());
 
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+app.use(limiter);
 app.use(errorController);
 
 app.use('/api/v1/auth', authRouter);
